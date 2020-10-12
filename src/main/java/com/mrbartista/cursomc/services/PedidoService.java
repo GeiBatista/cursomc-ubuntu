20b30c3f98fdb4c08c5c16dec53c81146ca2c453
GeiBatista <geibatista@hotmail.com>
2020-10-11 21:43:58 -0300

    Projeto criado no ubuntu

---
+++ src/main/java/com/mrbartista/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.mrbartista.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mrbartista.cursomc.domain.Pedido;
+import com.mrbartista.cursomc.repositories.PedidoRepository;
+import com.mrbartista.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
